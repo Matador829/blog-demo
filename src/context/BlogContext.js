0bb442dde5e645dd5749828c6f0e3c1b3e507163
Julio Buendia <buendia@stanford.edu>
2025-03-03 23:00:19 -0800

    Finish Edit and basic function

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -7,6 +7,10 @@ const blogReducer = (state, action) => {
             return [...state, { id: Math.floor(Math.random() * 99999), title: action.payload.title, content: action.payload.content }]
         case 'delete_blogpost':
             return state.filter((blogPost) => blogPost.id !== action.payload);
+        case 'edit_blogpost':
+            return state.map((blogPost) => {
+                return blogPost.id === action.payload.id ? action.payload : blogPost;
+            });
         default:
             return state;
     }
@@ -27,4 +31,13 @@ const deleteBlogPost = (dispatch) => {
     }
 }
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, []);
+const editBlogPost = (dispatch) => {
+    return (id, title, content, callback) => {
+        dispatch({ type: 'edit_blogpost', payload: { id, title, content } });
+        if (callback) {
+            callback();
+        }
+    }
+}
+
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost, editBlogPost }, [{ id: 1, title: 'Test Post', content: 'Test Content' , id: 1}]);
